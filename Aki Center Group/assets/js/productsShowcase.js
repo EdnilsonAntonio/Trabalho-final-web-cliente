49986e5f2f4d5b818457f8caaf113af51bce9fa6
EdnilsonAntonio <ednilsonantonio17403@gmail.com>
2024-05-06 21:15:28 +0100

    Adição dos restantes atributos dos produtos

--- Aki Center Group/assets/js/productsShowcase.js
+++ Aki Center Group/assets/js/productsShowcase.js
@@ -2,42 +2,58 @@
 
 // 1 - Lista de produtos
 const products = [
-    {
+  {
     // EA SPORTS FC 24
-      id: 1,
-      title: "EA SPORTS FC 24",
-      image: "/assets/img/cards/ea sports fc 24.jpg",
-      price: "36.000Kz",
-      category: "Gaming"
-    },
-    {
+    id: 1,
+    title: "EA SPORTS FC 24",
+    image: "/assets/img/cards/ea sports fc 24.jpg",
+    price: "36.000Kz",
+    category: "Gaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 5.0,
+    sales: 10,
+    releaseDate: new Date(2023, 8, 29)
+  },
+  {
     // Netflix: Plano Solo
-      id: 2,
-      title: "Netflix: Plano Solo",
-      image: "/assets/img/cards/netflix.jpeg",
-      price: "4.250Kz p/mês",
-      Category: "Streaming"
-    },
-    // Netflix: Plano Kamba
-    {
-        id: 3,
-        title: "Netflix: Plano Kamba",
-        image: "/assets/img/cards/netflix.jpeg",
-        price: "6.500Kz p/mês",
-        Category: "Streaming"
-    },
-    // Amazon Prime: Plano d'Ouro
-    {
-        id: 4,
-        title: "Prime Video: Plano d'Ouro",
-        image: "/assets/img/cards/prime video.png",
-        price: "6.500Kz p/mês",
-        Category: "Streaming"
-    }
-  ];
-  // 2 - Template do produto
-  const productCardTemplate = (product) => {
-    return `
+    id: 2,
+    title: "Netflix: Plano Solo",
+    image: "/assets/img/cards/netflix.jpeg",
+    price: "4.250Kz p/mês",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 5.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // Netflix: Plano Kamba
+  {
+    id: 3,
+    title: "Netflix: Plano Kamba",
+    image: "/assets/img/cards/netflix.jpeg",
+    price: "6.500Kz p/mês",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 5.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // Amazon Prime: Plano d'Ouro
+  {
+    id: 4,
+    title: "Prime Video: Plano d'Ouro",
+    image: "/assets/img/cards/prime video.png",
+    price: "6.500Kz p/mês",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 5.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  }
+];
+// 2 - Template do produto
+const productCardTemplate = (product) => {
+  return `
       <div class="col-lg-3 col-md-6 col-sm-12 card-container">
         <div class="card product-card">
           <img src="${product.image}" class="card-img-top" alt="${product.title}">
@@ -52,14 +68,14 @@ const products = [
         </div>
       </div>
     `;
-  };
-  // 3 - Função "generateProductCards": Serve para adicionar o produto na secção de produtos no HTML
-  const generateProductCards = () => {
-    products.sort(() => Math.random() - 0.5); // Baralhar a lista de produtos
-    const productCardsHTML = products.map(productCardTemplate).join("");
-    document.querySelector(".products").innerHTML = productCardsHTML;
-  };
-  // 4 - Chama a função assim que o documento for carregado
-  document.addEventListener("DOMContentLoaded", () => {
-    generateProductCards();
-  });
+};
+// 3 - Função "generateProductCards": Serve para adicionar o produto na secção de produtos no HTML
+const generateProductCards = () => {
+  const productCardsHTML = products.map(productCardTemplate).join("");
+  document.querySelector(".products").innerHTML = productCardsHTML;
+};
+// 4 - Chama a função assim que o documento for carregado
+document.addEventListener("DOMContentLoaded", () => {
+  generateProductCards();
+});
+// products.sort(() => Math.random() - 0.5); - Baralhar a lista de produtos
