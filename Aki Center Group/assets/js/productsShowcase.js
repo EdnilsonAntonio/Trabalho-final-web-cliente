de8c93c02800c35b5998b6af3472e1f15282a350
EdnilsonAntonio <ednilsonantonio17403@gmail.com>
2024-05-07 12:57:13 +0100

    Adição de novos produtos e altareção do box shadow

--- Aki Center Group/assets/js/productsShowcase.js
+++ Aki Center Group/assets/js/productsShowcase.js
@@ -2,24 +2,24 @@
 
 // 1 - Lista de produtos
 const products = [
+  // EA SPORTS FC 24
   {
-    // EA SPORTS FC 24
     id: 1,
     title: "EA SPORTS FC 24",
     image: "/assets/img/cards/ea sports fc 24.jpg",
-    price: "36.000Kz",
+    price: "36.000",
     category: "Gaming",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     rating: 5.0,
     sales: 10,
     releaseDate: new Date(2023, 8, 29)
   },
+  // Netflix: Plano Solo
   {
-    // Netflix: Plano Solo
     id: 2,
     title: "Netflix: Plano Solo",
     image: "/assets/img/cards/netflix.jpeg",
-    price: "4.250Kz p/mês",
+    price: "4.250 p/mês",
     Category: "Streaming",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     rating: 5.0,
@@ -31,7 +31,7 @@ const products = [
     id: 3,
     title: "Netflix: Plano Kamba",
     image: "/assets/img/cards/netflix.jpeg",
-    price: "6.500Kz p/mês",
+    price: "6.500 p/mês",
     Category: "Streaming",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     rating: 5.0,
@@ -43,12 +43,108 @@ const products = [
     id: 4,
     title: "Prime Video: Plano d'Ouro",
     image: "/assets/img/cards/prime video.png",
-    price: "6.500Kz p/mês",
+    price: "6.500 p/mês",
     Category: "Streaming",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    rating: 5.0,
+    rating: 4.0,
     sales: 10,
     releaseDate: new Date(2021, 3, 17)
+  },
+  // Call Of Duty: Modern Warfare III
+  {
+    id: 5,
+    title: "Call Of Duty: Modern Warfare III",
+    image: "/assets/img/cards/cod mw3.jpg",
+    price: "45.000",
+    Category: "Gaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2023, 1, 11)
+  },
+  // Amazon Prime: Plano Diamante
+  {
+    id: 6,
+    title: "Prime Video: Plano Diamante",
+    image: "/assets/img/cards/prime video.png",
+    price: "60.000 p/ano",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // HBO Max: Plano d'Ouro
+  {
+    id: 7,
+    title: "HBO Max: Plano d'Ouro",
+    image: "/assets/img/cards/hbomax.jpg",
+    price: "12.000 p/mês",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // Spotify: Plano Individual
+  {
+    id: 8,
+    title: "Spotify: Plano Individual",
+    image: "/assets/img/cards/spotify.jpeg",
+    price: "9.500 p/mês",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // HBO Max: Plano Diamante
+  {
+    id: 9,
+    title: "HBO Max: Plano d'Ouro",
+    image: "/assets/img/cards/hbomax.jpg",
+    price: "68.000 p/ano",
+    Category: "Streaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2021, 3, 17)
+  },
+  // eFootball: Bluelock
+  {
+    id: 10,
+    title: "eFootball: Bluelock",
+    image: "/assets/img/cards/efootball bluelock.jpg",
+    price: "45.000",
+    Category: "Gaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2021, 8, 20)
+  },
+  // MLB 24
+  {
+    id: 11,
+    title: "MLB 24",
+    image: "/assets/img/cards/mlb24.jpg",
+    price: "45.000",
+    Category: "Gaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2024, 2, 19)
+  },
+  // Rise of the ronin
+  {
+    id: 12,
+    title: "Rise of the ronin",
+    image: "/assets/img/cards/rise of the ronin.jpg",
+    price: "45.000",
+    Category: "Gaming",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    rating: 4.0,
+    sales: 10,
+    releaseDate: new Date(2024, 2, 22)
   },
 ];
 // 2 - Template do produto
@@ -59,7 +155,7 @@ const productCardTemplate = (product) => {
           <img src="${product.image}" class="card-img-top" alt="${product.title}">
           <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
-            <p class="card-text">${product.price}</p>
+            <p class="card-text">Kz${product.price}</p>
             <div class="card-actions">
               <a href="" class="btn btn-primary">Obter agora</a>
               <button class="btn product-cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></button>
