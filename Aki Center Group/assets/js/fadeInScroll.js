576f59f872e0008d70fdd8434f0c27ec4218971b
EdnilsonAntonio <ednilsonantonio17403@gmail.com>
2024-05-15 21:24:25 +0100

    Revelar conteúdo ao rolar

---
+++ Aki Center Group/assets/js/fadeInScroll.js
@@ -0,0 +1,22 @@
+// Script: Este script serve para revelar o conteúdo da página ao rolar com ajuda do CSS
+
+window.addEventListener('scroll', reveal)
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+
+        var windowheight = window.innerHeight
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('reveal-now');
+        }
+        else {
+            reveals[i].classList.remove('reveal-now');
+        }
+    }
+
+}
